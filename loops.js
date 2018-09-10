863f2450af868c2ab201f1b95dfe66e00fa75d7d
kgravesjohnson@gmail.com <kgravesjohnson@gmail.com>
2018-09-10 21:55:49 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,11 +1,11 @@
 function forLoop (array) {
   for (let i = ; i < 26; i++) {
     if (i === 0) {
-      array[i] = 'I am 1 strange loop.'
+      array[i] = `I am ${1} strange loop.`;
     } else
     array[i] = `I am ${i} strange loops.`;
   }
   
 }
   
-  // change 25 to 26 and i++ to ++i
+  
